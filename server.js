d47b3d4e3464ada0b735bef2e74ddd9b4ddc28f3
Joao Ferreira <yferreirajoao@gmail.com>
2024-11-19 12:42:24 -0300

    mock json

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 import express from 'express'
 
+const posts = [
+    {id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150"},
+    {id: 2, descricao: "Gato Yoga", imagem: "https://placecats.com/millie/300/150"},
+    {id: 3, descricao: "Gato Panqueca", imagem: "https://placecats.com/millie/300/150"}
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get('/api', (req, res) => {
-    res.status(200).send("Pão de Alho");
+app.get('/posts', (req, res) => {
+    res.status(200).json(posts);
+});
+
+function BuscarPostPorId(id){
+    return posts.findIndex((posts) => {
+        return posts.id === Number(id)
+    });
+};
+
+app.get('/posts/:id', (req, res) => {
+    const index = BuscarPostPorId(req.params.id);
+    res.status(200).json(posts[index]);
 });
