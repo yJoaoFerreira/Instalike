f3e774d7fc79348e39773c72286c329e34b4d9eb
Joao Ferreira <yferreirajoao@gmail.com>
2024-11-21 13:26:33 -0300

    Add multer; Armazenamento e Upload de Imagens.

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,36 @@
-import getTodosPosts from "../models/postModel.js";
+import { getTodosPosts, criarPost } from "../models/postModel.js";
+import fs from 'fs';
 
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts();
     res.status(200).json(posts);
+};
+
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+    try {
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro" : "Falha na requisição"});
+    };
+};
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada);
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro" : "Falha na requisição"});
+    };
 };
--- src/models/postModel.js
+++ src/models/postModel.js
@@ -2,8 +2,14 @@ import conectarAoBanco from '../config/dbconfig.js';
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     const db = conexao.db("instalike");
     const colecao = db.collection("posts");
     return colecao.find().toArray();
+};
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("instalike");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
 };
