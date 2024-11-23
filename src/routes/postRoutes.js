d6b675f76129e2cb1601e747e07c06c5d189e833
Joao Ferreira <yferreirajoao@gmail.com>
2024-11-22 21:27:43 -0300

    Conexão com o front

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import multer from 'multer';
+import cors from 'cors';
 import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from '../controllers/postsController.js';
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+};
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -15,6 +21,7 @@ const upload = multer({ dest: "./uploads" , storage});
 
 const routes = (app) => {
     app.use(express.json());
+    app.use(cors(corsOptions));
 
     app.get('/posts', listarPosts);
     app.post('/posts', postarNovoPost);
